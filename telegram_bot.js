39ac402ddaaebccee71882b4aa6d2d8c3203e64e
mateusspp <teu.patricio@hotmail.com>
2022-04-09 12:53:31 -0300

    Adicionando botões e tentanto ajeitar package.json

--- telegram_bot.js
+++ telegram_bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = 'ff';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -17,6 +17,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
+
 });
 
 // Listen for any kind of message. There are different kinds of
@@ -25,5 +26,43 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, "Escolha uma das opções abaixo:", {
+    "reply_markup": {
+      "inline_keyboard": [
+        [
+          {
+            text: "Criar um usuário",
+            callback_data: "click",
+          },
+        ],
+        [
+          {
+            text: "Deletar um usuário",
+            callback_data: "click2",
+          },
+        ],
+        [
+          {
+            text: "Quantidade total de usuários",
+            callback_data: "click3",
+          },
+        ],
+        [
+          {
+            text: "Quantidade de usuários online",
+            callback_data: "click4",
+          },
+        ],
+        [
+          {
+            text: "Acessar diretamente o painel",
+            callback_data: "click5",
+            url: `https://stackoverflow.com/questions/61467722/difference-between-npm-update-and-remove-package-lock-json-plus-npm-install`,
+          },
+        ],
+      ],
+      
+    },
+    
+  });
 });
