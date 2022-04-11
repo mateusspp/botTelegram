0c590b234f7e60a12c4d16ec3ccce80026d89eb5
mateusspp <teu.patricio@hotmail.com>
2022-04-11 00:34:46 -0300

    Adicionando retorno de todos os clicks

--- telegram_bot.js
+++ telegram_bot.js
@@ -17,10 +17,10 @@ bot.on('message', (msg) => {
       "inline_keyboard": [
         [{
             text: "Criar um usuário",
-            callback_data: "teste azul",},],
+            callback_data: "click1",},],
         [{
             text: "Deletar um usuário",
-            callback_data: "oi",},],
+            callback_data: "click2",},],
         [{
             text: "Atualizar dados de um usuário",
             callback_data: "click3",},],
@@ -41,16 +41,40 @@ bot.on('message', (msg) => {
   });
 });
 
+function teste (callback_data) {
+  if (callback_data == "click3") {
+    return console.log("tudo zuado")
+  } else {
+    return console.log("deu certo tbm, mas eu n cliquei")
+  }
+}
 
+//colocar botao de atualizar dados de usuario, aí vira uma api rest/crud =)
 bot.on('callback_query', (carro) => {
   const msg = carro.message;
+  //criar uma variavel que guarda carro data e retornar ela. ai uso numa função?
   console.log(carro.data);
-  if(carro.data == 'click3') {
+  if(carro.data == 'click1') {
     bot.answerCallbackQuery(carro.id)
-      .then(() => bot.sendMessage(msg.chat.id, "Deu certo!"));
+      .then(() => bot.sendMessage(msg.chat.id, "Primeiro click deu certo!"));
+  } else if (carro.data == 'click2') {
+    bot.answerCallbackQuery(carro.id)
+    .then(() => bot.sendMessage(msg.chat.id, "Segundo click deu certo!"));
+  } else if (carro.data == 'click3') {
+    bot.answerCallbackQuery(carro.id)
+    .then(() => bot.sendMessage(msg.chat.id, "Terceiro click deu certo!"));
+  } else if (carro.data == 'click4') {
+    bot.answerCallbackQuery(carro.id)
+    .then(() => bot.sendMessage(msg.chat.id, "Quarto click deu certo!"));
+  } else if (carro.data == 'click5') {
+    bot.answerCallbackQuery(carro.id)
+    .then(() => bot.sendMessage(msg.chat.id, "Quinto click deu certo!"));
+  } else if (carro.data == 'click6') {
+    bot.answerCallbackQuery(carro.id)
+    .then(() => bot.sendMessage(msg.chat.id, "Sexto click deu certo!"));
   } else {
     bot.answerCallbackQuery(carro.id)
-    .then(() => bot.sendMessage(msg.chat.id, "Deu certo seu commit!"));
+    .then(() => bot.sendMessage(msg.chat.id, "Algum erro ocorreu!"));
   }
 });
 
